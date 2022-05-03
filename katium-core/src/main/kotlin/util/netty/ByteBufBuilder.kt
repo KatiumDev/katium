8de2daf0d79aa6d613753e18dd79415be1ed49aa
xtexChooser <zbwxtex@qq.com>
2022-05-03 21:19:01 +0800

    feat: wrappers for ByteBuf

--- katium-core/src/main/kotlin/util/netty/ByteBufBuilder.kt
+++ katium-core/src/main/kotlin/util/netty/ByteBufBuilder.kt
@@ -2,6 +2,7 @@ package katium.core.util.netty
 
 import io.netty.buffer.ByteBuf
 import io.netty.buffer.ByteBufAllocator
+import java.io.InputStream
 
 inline fun ByteBufAllocator.buffer(crossinline writer: ByteBuf.() -> Unit): ByteBuf = buffer().apply(writer)
 inline fun ByteBufAllocator.heapBuffer(crossinline writer: ByteBuf.() -> Unit): ByteBuf = heapBuffer().apply(writer)
@@ -9,3 +10,16 @@ inline fun ByteBufAllocator.compositeHeapBuffer(crossinline writer: ByteBuf.() -
 inline fun ByteBufAllocator.compositeBuffer(crossinline writer: ByteBuf.() -> Unit): ByteBuf = compositeBuffer().apply(writer)
 inline fun ByteBufAllocator.directBuffer(crossinline writer: ByteBuf.() -> Unit): ByteBuf = directBuffer().apply(writer)
 inline fun ByteBufAllocator.ioBuffer(crossinline writer: ByteBuf.() -> Unit): ByteBuf = ioBuffer().apply(writer)
+
+fun ByteBufAllocator.buffer(bytes: ByteArray): ByteBuf = buffer(bytes.size).writeBytes(bytes)
+fun ByteBufAllocator.buffer(input: InputStream): ByteBuf = buffer(input.available()).writeBytes(input.readAllBytes())
+fun ByteBufAllocator.heapBuffer(bytes: ByteArray): ByteBuf = heapBuffer(bytes.size).writeBytes(bytes)
+fun ByteBufAllocator.heapBuffer(input: InputStream): ByteBuf = heapBuffer(input.available()).writeBytes(input.readAllBytes())
+fun ByteBufAllocator.compositeHeapBuffer(bytes: ByteArray): ByteBuf = compositeHeapBuffer(bytes.size).writeBytes(bytes)
+fun ByteBufAllocator.compositeHeapBuffer(input: InputStream): ByteBuf = compositeHeapBuffer(input.available()).writeBytes(input.readAllBytes())
+fun ByteBufAllocator.compositeBuffer(bytes: ByteArray): ByteBuf = compositeBuffer(bytes.size).writeBytes(bytes)
+fun ByteBufAllocator.compositeBuffer(input: InputStream): ByteBuf = compositeBuffer(input.available()).writeBytes(input.readAllBytes())
+fun ByteBufAllocator.directBuffer(bytes: ByteArray): ByteBuf = directBuffer(bytes.size).writeBytes(bytes)
+fun ByteBufAllocator.directBuffer(input: InputStream): ByteBuf = directBuffer(input.available()).writeBytes(input.readAllBytes())
+fun ByteBufAllocator.ioBuffer(bytes: ByteArray): ByteBuf = ioBuffer(bytes.size).writeBytes(bytes)
+fun ByteBufAllocator.ioBuffer(input: InputStream): ByteBuf = ioBuffer(input.available()).writeBytes(input.readAllBytes())
