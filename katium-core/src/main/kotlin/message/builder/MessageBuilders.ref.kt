85b77e8eb48a0f226a079aa694f6fd581f04bb38
xtexChooser <zbwxtex@qq.com>
2022-06-04 21:31:52 +0800

    feat(core): ref message & quote reply

---
+++ katium-core/src/main/kotlin/message/builder/MessageBuilders.ref.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022 Katium Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+@file:Suppress("FunctionName")
+@file:JvmName("Ats")
+
+package katium.core.message.builder
+
+import katium.core.message.Message
+import katium.core.message.MessageRef
+import katium.core.message.content.MessageContent
+import katium.core.message.content.QuoteReply
+import katium.core.message.content.RefMessage
+
+@JvmName("ofRef")
+fun Ref(ref: MessageRef) = RefMessage(ref)
+
+@JvmName("ofRef")
+fun Ref(message: Message) = RefMessage(message.ref)
+
+@JvmName("ofReply")
+fun Reply(ref: MessageRef) = QuoteReply(ref)
+
+@JvmName("ofReply")
+fun Reply(message: Message) = QuoteReply(message.ref)
+
+operator fun MessageContent.plus(ref: MessageRef) = merge(RefMessage(ref))
+operator fun MessageContent.plus(message: Message) = merge(Reply(message))
---
+++ katium-core/src/main/kotlin/message/content/RefMessage.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2022 Katium Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package katium.core.message.content
+
+import katium.core.message.MessageRef
+
+open class RefMessage(val ref: MessageRef) : MessageContent() {
+
+    override fun simplify() = null
+    override fun concat(other: MessageContent) = null
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is RefMessage) return false
+        if (ref != other.ref) return false
+        return true
+    }
+
+    override fun hashCode() = ref.hashCode()
+
+    override fun asString() = "~$ref "
+    override fun toString() = "[~$ref]"
+
+}
